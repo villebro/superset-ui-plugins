2a003c4dee31cef8487e70e15525c5531459a2e3
Conglei Shi <conglei.shi@airbnb.com>
2019-05-20 13:43:30 -0700

    feat(tablevis): this pr is to add a new tablevis plguin to the system

--- packages/superset-ui-plugins-demo/.storybook/webpack.config.js
+++ packages/superset-ui-plugins-demo/.storybook/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const BABEL_TYPESCRIPT_OPTIONS = {
   presets: [
-    ['@babel/preset-env', { useBuiltIns: 'entry' }],
+    ['@babel/preset-env', { useBuiltIns: 'entry', corejs: 3 }],
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
@@ -48,6 +48,38 @@ module.exports = async ({ config }) => {
       options: BABEL_TYPESCRIPT_OPTIONS,
     }],
   });
+
+  delete config.resolve.alias['core-js'];
+
+  const coreJS3Overrides = [
+    'web.dom-collections.iterator',
+    'es.number.to-fixed',
+    'es.promise',
+    'es.promise.finally',
+    'es.regexp.constructor',
+    'es.regexp.to-string',
+    'es.string.match',
+    'es.string.replace',
+    'es.string.split',
+  ];
+
+  coreJS3Overrides.forEach(name => {
+    config.resolve.alias[`core-js/modules/${name}`] = path.resolve(
+      __dirname,
+      `../../../node_modules/core-js/modules/${name}`,
+    );
+  });
+
+  // point _storybook_ to core-js@2
+  config.resolve.alias['core-js/modules'] = path.resolve(
+    __dirname,
+    '../../../node_modules/@storybook/core/node_modules/core-js/modules',
+  );
+
+  // config.resolve.alias['@airbnb/lunar'] = path.resolve(
+  //   __dirname,
+  //   '../../../node_modules/lunar/packages/core',
+  // );
 
   return config;
 };
